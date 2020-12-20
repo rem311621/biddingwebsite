84de1e8714decbad7599508c89090fc5e35eac5e
bang bang <you@example.com>
2020-12-20 16:27:38 +0800

    update

--- server/dist/app.dev.js
+++ server/dist/app.dev.js
@@ -5,7 +5,7 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || "5000";
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello World Gay");
 });
 app.listen(PORT, function () {
   console.log("Server start at ".concat(PORT));
